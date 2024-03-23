d345f85d840e80a5adf607b03c7189c4d53b005b
Bruno Bulnes <bulnes.bruno@gmail.com>
2024-03-23 19:46:29 +0000

    feat: header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,20 @@
+import logo from "../../assets/logo.svg";
+
+import styles from "./Header.module.css";
+
 export function Header() {
   return (
-    <header>
-      <h1>Header</h1>
+    <header className={styles.header}>
+      <h1>
+        <img
+          src={logo}
+          alt="ToDo Rocket"
+          width={126}
+          height={48}
+          loading="lazy"
+        />
+        <span className="sr-only">ToDo Rocket</span>
+      </h1>
     </header>
   );
 }
